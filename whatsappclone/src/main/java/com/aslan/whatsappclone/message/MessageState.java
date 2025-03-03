54edaf7154adc59572d30c45a500524888a2bfdf
Aslan Halil <AslanCHalil@outlook.com>
2025-03-03 18:02:26 +0200

    Created entities and named queries.

---
+++ whatsappclone/src/main/java/com/aslan/whatsappclone/message/MessageState.java
@@ -0,0 +1,6 @@
+package com.aslan.whatsappclone.message;
+
+public enum MessageState {
+    SENT,
+    SEEN
+}
---
+++ whatsappclone/src/main/java/com/aslan/whatsappclone/message/MessageType.java
@@ -0,0 +1,8 @@
+package com.aslan.whatsappclone.message;
+
+public enum MessageType {
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+}
